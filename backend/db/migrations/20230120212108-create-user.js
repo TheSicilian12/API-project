52ca21c51dcfa37bf14a2f8c5ad6e78365acbbcc
TheSicilian12 <mguidera314@gmail.com>
2023-01-20 17:10:34 -0500

    migration edit

--- backend/db/migrations/20230120212108-create-user.js
+++ backend/db/migrations/20230120212108-create-user.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const { options } = require('../../routes');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -33,9 +36,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    options.tableName = "User";
+    await queryInterface.dropTable(options);
   }
 };
