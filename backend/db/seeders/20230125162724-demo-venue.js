2dc2b2fced6574533bf347f4f6b98030384c2308
TheSicilian12 <mguidera314@gmail.com>
2023-01-25 13:21:26 -0500

    updated seeder file for memberships

--- backend/db/seeders/20230125162724-demo-venue.js
+++ backend/db/seeders/20230125162724-demo-venue.js
@@ -1,6 +1,14 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+options.tableName = 'Venues';
+
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -12,6 +20,37 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    options.tableName = "Venues";
+    return queryInterface.bulkInsert(options, [
+      {
+        address: '1111 road A',
+        city: 'Denver',
+        state: 'Colorado',
+        lat: 111.1,
+        lng: 222.2,
+      },
+      {
+        address: '2222 road B',
+        city: 'Columbus',
+        state: 'Ohio',
+        lat: 1212.1,
+        lng: 4545.2,
+      },
+      {
+        address: '3333 road C',
+        city: 'Detroit',
+        state: 'Michigan',
+        lat: 7878.1,
+        lng: 8787.2,
+      },
+      {
+        address: '4444 road D',
+        city: 'Dayton',
+        state: 'Ohio',
+        lat: 31.1,
+        lng: 654.2,
+      },
+    ], {})
   },
 
   async down (queryInterface, Sequelize) {
@@ -21,5 +60,10 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    options.tableName = 'Venues';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      state: { [Op.in]: ['Ohio', 'Michigan', 'Colorado'] }
+    }, {});
   }
 };
