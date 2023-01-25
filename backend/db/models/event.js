230d0057d32cdde85222cda8debbb2840a790608
TheSicilian12 <mguidera314@gmail.com>
2023-01-25 12:45:11 -0500

    association update to include through tables

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Event.hasMany(models.EventImage, {foreignKey: 'eventId'})
-      Event.hasMany(models.Attendance, {foreignKey: 'eventId'})
+      Event.belongsToMany(models.Users, {through: models.Attendance})
 
       Event.belongsTo(models.Venue, {foreignKey: 'venueId'})
       Event.belongsTo(models.Group, {foreignKey: 'groupId'})
