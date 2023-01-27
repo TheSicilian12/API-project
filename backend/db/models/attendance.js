f7caf6ec42a196cb28edfda9c97f3e07b3368f72
TheSicilian12 <mguidera314@gmail.com>
2023-01-27 11:38:47 -0500

    edit enum attendance

--- backend/db/models/attendance.js
+++ backend/db/models/attendance.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    status: DataTypes.ENUM ("attending", "not attending"),
+    status: DataTypes.ENUM ("test1", "test2"),
   }, {
     sequelize,
     modelName: 'Attendance',
