6f8f584c435f96b54d70acb6b344e6e699aa4130
TheSicilian12 <mguidera314@gmail.com>
2023-02-01 15:35:57 -0500

    testing live server error: '

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -31,29 +31,29 @@ router.get('/', async (req, res) => {
         eventObj.Events.push(event.toJSON())
     }
 
-    //preview image add
-    for (let e of eventObj.Events) {
-        // console.log(e.EventImages)
-        if (e.EventImages[0]) {
-            e.previewImage = e.EventImages[0].url
-        }
-        delete e.EventImages
-    }
+    // //preview image add
+    // for (let e of eventObj.Events) {
+    //     // console.log(e.EventImages)
+    //     if (e.EventImages[0]) {
+    //         e.previewImage = e.EventImages[0].url
+    //     }
+    //     delete e.EventImages
+    // }
 
-    //num attending add
-    for (let e of eventObj.Events) {
-        if (e.Attendances.length) {
-            let count = []
-            for (let attend of e.Attendances) {
-                // console.log(attend.status)
-                if (attend.status === "test1") { // THIS WOULD BE WHERE AN ENUM CHANGE WOULD TAKE PLACE
-                    count.push(attend)
-                }
-            }
-            e.numAttending = count.length
-        } else e.numAttending = 0;
-        delete e.Attendances
-    }
+    // //num attending add
+    // for (let e of eventObj.Events) {
+    //     if (e.Attendances.length) {
+    //         let count = []
+    //         for (let attend of e.Attendances) {
+    //             // console.log(attend.status)
+    //             if (attend.status === "test1") { // THIS WOULD BE WHERE AN ENUM CHANGE WOULD TAKE PLACE
+    //                 count.push(attend)
+    //             }
+    //         }
+    //         e.numAttending = count.length
+    //     } else e.numAttending = 0;
+    //     delete e.Attendances
+    // }
 
     res.json(eventObj)
 
