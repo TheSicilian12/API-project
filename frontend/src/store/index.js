bff7bd0879c65a6256501d9e0acc06228b11cfda
TheSicilian12 <mguidera314@gmail.com>
2023-03-10 13:56:41 -0500

    part 1

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,16 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+// ...
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-  // add reducer functions here
+  session: sessionReducer,
 });
+// ...
+
+// const rootReducer = combineReducers({
+//   // add reducer functions here
+// });
 
 let enhancer;
 
