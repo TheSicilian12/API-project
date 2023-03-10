d89c350779a77e560a09d0d256a8dd797591c4fb
TheSicilian12 <mguidera314@gmail.com>
2023-03-10 13:32:32 -0500

    auth me phase 0

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const apiRouter = require('./api');
+// const apiRouter = require('./api');
 
 
 // router.get('/hello/world', function(req, res) {
@@ -11,14 +11,16 @@ const apiRouter = require('./api');
 // });
 
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
   });
+});
 
+const apiRouter = require('./api');
 router.use('/api', apiRouter);
+// router.use('/api', apiRouter);
 
 // backend/routes/index.js
 // ... after `router.use('/api', apiRouter);`
