29212ced1544e4ee1064ebbd40389cee970ba521
TheSicilian12 <mguidera314@gmail.com>
2023-03-10 19:04:30 -0500

    updated scripts for render

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,29 +1,8 @@
-// backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
-// const apiRouter = require('./api');
-
-
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
-
 const apiRouter = require('./api');
-router.use('/api', apiRouter);
-// router.use('/api', apiRouter);
 
-// backend/routes/index.js
-// ... after `router.use('/api', apiRouter);`
+router.use('/api', apiRouter);
 
 // Static routes
 // Serve React build files in production
@@ -32,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
@@ -43,19 +22,17 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'build', 'index.html')
     );
   });
 }
 
-// ...
-
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+    res.status(201).json({});
   });
 }
 
