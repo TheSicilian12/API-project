62b85edc1b820fed3481c29e8d261c525ca138bf
TheSicilian12 <mguidera314@gmail.com>
2023-03-11 10:32:59 -0500

    all groups page update

--- frontend/src/components/AllGroups/index.js
+++ frontend/src/components/AllGroups/index.js
@@ -3,11 +3,21 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './AllGroups.css';
 
-function AllGroups({ isLoaded }){
+function AllGroups({ isLoaded }) {
 
   return (
-    <div>
-        All Groups test
+    <div className='GroupsPage'>
+      <div className='GroupsPage_eventGroupLinks'>
+        <NavLink className = 'GroupPage_eventGroupLinks_Event' to='/events'>
+          Events
+        </NavLink>
+        <NavLink className = 'GroupPage_eventGroupLinks_Group' to='/groups'>
+          Groups
+        </NavLink>
+      </div>
+      <p className='GroupsPage_groupsListHeader'>
+        Groups in Meetup
+      </p>
     </div>
   );
 }
