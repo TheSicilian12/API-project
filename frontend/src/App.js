7cae33c7f26c81fd56ad1d618322dd3c5e7439f5
TheSicilian12 <mguidera314@gmail.com>
2023-03-11 10:26:13 -0500

    all groups page connected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import LandingPage from "./components/LandingPage";
+import AllGroups from "./components/AllGroups";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,6 +20,9 @@ function App() {
         <Switch>
           <Route path='/' exact>
             <LandingPage />
+          </Route>
+          <Route path='/groups' exact>
+            <AllGroups />
           </Route>
         </Switch>
       )}
---
+++ frontend/src/components/AllGroups/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import './AllGroups.css';
+
+function AllGroups({ isLoaded }){
+
+  return (
+    <div>
+        All Groups test
+    </div>
+  );
+}
+
+export default AllGroups;
