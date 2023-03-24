a020db09954fb64644659815b198535b757b9faa
TheSicilian12 <mguidera314@gmail.com>
2023-03-24 14:11:32 -0400

    working on image

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -6,29 +6,29 @@ import SeeAllGroups from './SeeAllGroups';
 import StartGroup from './StartGroup';
 import './LandingPage.css';
 import flowerImage from '../assets/Images/Example.jpg';
+import landingPageInfo from '../assets/Images/pngfind.com-hora-de-aventura-png-6738376.png';
 
 function LandingPage() {
     return (
         <>
             <div className='landingPage_intro borderRed displayFlex justifyCenter'>
-                <div>
-                    <h1>
+                <div className='borderBlack displayFlex flex-directionColumn'>
+                    <h1 className='textWrap borderRed'>
                         The people platform - Where skills become monster slaying, problem solving adventuring parties:
                     </h1>
-                    <p>
+                    <p className='textWrap borderRed'>
                         Whatever your skills, from heavy damage deeling and herbal remedies to political intrigue and assasinations,
                         there are thousands of people who could use your expertise on Adventureup.
                         Quests are happening every day—log in to join the adventure!
                     </p>
                 </div>
-                <div>
-                    <img
-                        src={flowerImage}
+                {/* <div className='displayFlex justifyCenter'> */}
+                    <img className='borderGreen displayFlex'
+                        src={landingPageInfo}
                         alt="test image"
-                        // width="400"
-                        // height="341"
+                        width='20%'
                         />
-                </div>
+                {/* </div> */}
             </div>
             <div className='landingPage_explaination borderBlack displayFlex justifyCenter'>
                 How Meetup works: ...
