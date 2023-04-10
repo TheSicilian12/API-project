eebaf855742f73d1edca8ba03c72b834b219793f
TheSicilian12 <mguidera314@gmail.com>
2023-04-10 11:53:55 -0400

    groupImage migration and model files updated from STRING to TEXT

--- frontend/src/store/groupsThunk.js
+++ frontend/src/store/groupsThunk.js
@@ -268,6 +268,7 @@ const initialState = {
         // In this slice we have much more info about the event than in the allEvents slice. singleEvent: { eventData, Group: { groupData, }, // Note that venue here will have more information than venue did in the all events slice. (Refer to your API Docs for more info) Venue: { venueData, }, EventImages: [imagesData], // These would be extra features, not required for your first 2 CRUD features Members: [membersData], Attendees: [attendeeData], }, }, };
     }
 }
+
 //reducer - group reducer
 const groupReducer = (state = initialState, action) => {
     switch (action.type) {
