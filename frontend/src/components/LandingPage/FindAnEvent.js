38b64338af79a70343ad196fa879d80828bad095
TheSicilian12 <mguidera314@gmail.com>
2023-04-13 18:44:58 -0400

    working on landing page hover

--- frontend/src/components/LandingPage/FindAnEvent.js
+++ frontend/src/components/LandingPage/FindAnEvent.js
@@ -5,15 +5,21 @@ import './LandingPage.css';
 import eventImage from '../assets/Images/Daco_1979729.png';
 
 function FindAnEvent() {
+
+    let decoration = 'noDecoration'
+
+
     return (
-        <NavLink className='landingPage_findEvent_mainText borderRed pointerCursor' to='/events'>
-            <div className='landingPage_findEvent displayFlex flex-directionColumn alignCenter borderBlack pointerCursor'>
+        <NavLink
+        className={`landingPage_findEvent_mainText borderRed pointerCursor ${decoration}`} to='/events'>
+            <div
+            className='landingPage_findEvent displayFlex flex-directionColumn alignCenter borderBlack pointerCursor'>
                 <img className='borderRed'
                     src={eventImage}
                     alt="test image"
                     width='50%'
                 />
-                <h2>
+                <h2 className={`test`}>
                 Find an event
                 </h2>
                 {/* <p className='landingPage_findEvent_additionalText'>
