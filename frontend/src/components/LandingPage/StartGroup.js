769ece61128e8dced6511345e763de6a1eaa849f
TheSicilian12 <mguidera314@gmail.com>
2023-04-13 19:05:43 -0400

    link switch for enabling and disabling landing page link

--- frontend/src/components/LandingPage/StartGroup.js
+++ frontend/src/components/LandingPage/StartGroup.js
@@ -5,17 +5,23 @@ import './StartGroup.js';
 import './LandingPage.css';
 import startGroupImage from '../assets/Images/Daco_6030033.png';
 
+function StartGroup({ user }) {
 
-function StartGroup() {
+    let linkSwitch = 'enable';
+    if (!user) {
+        linkSwitch = 'disable'
+    }
+
     return (
-        <NavLink to='/groups/new' className='landingPage_startGroup_mainText pointerCursor noDecoration'>
+        <NavLink to='/groups/new' className={`landingPage_startGroup_mainText pointerCursor ${linkSwitch}`}>
             <div className='landingPage_startGroup displayFlex flex-directionColumn pointerCursor alignCenter'>
                 <img className=''
+
                     src={startGroupImage}
                     alt="test image"
                     width='50%'
                 />
-                <h2 className='color'>
+                <h2 className={linkSwitch}>
                     Start a new group
                 </h2>
                 {/* <p className='landingPage_startGroup_additionalText'>
@@ -23,6 +29,8 @@ function StartGroup() {
                 </p> */}
             </div>
         </NavLink>
+
+
     )
 }
 
