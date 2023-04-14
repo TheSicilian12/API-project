396f56a4e0747c6db81c3411aae6d6ca60f1168c
TheSicilian12 <mguidera314@gmail.com>
2023-04-14 16:54:46 -0400

    logo

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import '../UniversalCSS.css'
+import adventureUpLogo from '../assets/Images/AdvenutreUpLogo/adventureupplaceit-transparentedit.png'
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
+  const history = useHistory();
 
-  const user = useSelector((state) => state.session.user)
+  const user = useSelector((state) => state.session.user);
 
   // console.log('user: ', user)
 
   return (
       <div className='displayFlex justfiySpaceBetween alignCenter'>
-        <div className='home'>
-          <NavLink exact to="/" className='adventureUpFont'>Adventureup</NavLink>
+        <div className='home pointerCursor'>
+          {/* <NavLink exact to="/" className='adventureUpFont'>Adventureup</NavLink> */}
+          <img
+            width='50%'
+            onClick={() => {history.push('/')}}
+            src={adventureUpLogo}
+          />
         </div>
         <div className='borderBlack displayFlex paddingRight'>
           <div className='displayFlex alignCenter paddingRightStartNewGroup'>
