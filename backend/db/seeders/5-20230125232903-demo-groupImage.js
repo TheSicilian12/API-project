2d4a8c717856215aba409db5970083e91aa1e457
TheSicilian12 <mguidera314@gmail.com>
2023-04-16 19:23:28 -0400

    saving image chagnes seeder file

--- backend/db/seeders/5-20230125232903-demo-groupImage.js
+++ backend/db/seeders/5-20230125232903-demo-groupImage.js
@@ -24,43 +24,38 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         groupId: 1,
-        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa5SjjulTpiXxKuLHh9mlEGVmun-IYoI5YtQ&usqp=CAU',
+        url: 'https://media.wired.com/photos/593266104dc9b45ccec5da4b/master/w_2560%2Cc_limit/AdventureTime.jpg',
         preview: true,
       },
       {
         groupId: 2,
-        url: 'https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/3/3b/Jakesalad.png/revision/latest?cb=20190807133015',
-        preview: false,
+        url: 'https://cdn.wallpapersafari.com/89/71/vYaoRk.jpg',
+        preview: true,
       },
       {
         groupId: 3,
-        url: 'https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/0/00/Princess_Bubblegum.png/revision/latest?cb=20190807133134',
-        preview: false,
+        url: 'https://pyxis.nymag.com/v1/imgs/a78/7d6/96c5f3f236ec79b9c33325c97abb7c17cb-adventure-time-distant-lands-bmo.rsquare.w700.jpg',
+        preview: true,
       },
       {
         groupId: 4,
-        url: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d32b7d0a-208a-4fd6-afff-e19b964bb606/d5f3z7r-a5ba1c4f-ce81-4555-bb47-7377986061ac.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2QzMmI3ZDBhLTIwOGEtNGZkNi1hZmZmLWUxOWI5NjRiYjYwNlwvZDVmM3o3ci1hNWJhMWM0Zi1jZTgxLTQ1NTUtYmI0Ny03Mzc3OTg2MDYxYWMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.pNdxZCMcTjH3N-t6wASl9HlGkp9d4bE6gC3Q61lGlcI',
+        url: 'https://img.buzzfeed.com/buzzfeed-static/static/2022-09/22/18/asset/e217d4d915fe/sub-buzz-4558-1663869910-18.jpg',
         preview: true,
       },
       {
-        groupId: 10,
-        url: 'https://www.looper.com/img/gallery/why-ice-king-is-the-most-tragic-character-in-adventure-time/l-intro-1619451691.jpg',
+        groupId: 5,
+        url: 'https://media.bantmag.com/wp-content/uploads/a/adventure-time-with-finn-and-jake-5191e35a3afea-800x533.jpg',
         preview: true,
       },
       {
-        groupId: 10,
-        url: 'https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/ef/Ice_King%27s_Drum_Set.png/revision/latest/scale-to-width-down/640?cb=20110411215509',
-        preview: false,
+        groupId: 6,
+        url: 'https://i.pinimg.com/originals/ef/5b/e3/ef5be3494088f7a732b2f0773b0b8361.jpg',
+        preview: true,
       },
       {
-        groupId: 10,
-        url: 'https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/ef/Ice_King%27s_Drum_Set.png/revision/latest/scale-to-width-down/640?cb=20110411215509',
-        preview: false,
-      },
-      {
-        groupId: 10,
-        url: 'https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/ef/Ice_King%27s_Drum_Set.png/revision/latest/scale-to-width-down/640?cb=20110411215509',
-        preview: false,
+        groupId: 7,
+        url: 'https://upload.wikimedia.org/wikipedia/en/4/48/Adventure_Time_-_Marceline.png',
+        preview: true,
       },
       {
         groupId: 9,
@@ -80,6 +75,21 @@ module.exports = {
       {
         groupId: 9,
         url: 'https://i.ytimg.com/vi/3uMAgcBzItQ/maxresdefault.jpg',
+        preview: false,
+      },
+      {
+        groupId: 10,
+        url: 'https://www.looper.com/img/gallery/why-ice-king-is-the-most-tragic-character-in-adventure-time/l-intro-1619451691.jpg',
+        preview: true,
+      },
+      {
+        groupId: 10,
+        url: 'https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/ef/Ice_King%27s_Drum_Set.png/revision/latest/scale-to-width-down/640?cb=20110411215509',
+        preview: false,
+      },
+      {
+        groupId: 10,
+        url: 'https://static.wikia.nocookie.net/adventuretimewithfinnandjake/images/e/ef/Ice_King%27s_Drum_Set.png/revision/latest/scale-to-width-down/640?cb=20110411215509',
         preview: false,
       }
     ], {})
