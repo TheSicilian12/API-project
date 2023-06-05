7e9584249ac6384b4d42d6fdab56a5edbbd68a90
TheSicilian12 <mguidera314@gmail.com>
2023-06-05 14:01:49 -0400

    basic group adjustment

--- frontend/src/components/GroupDetails/index.js
+++ frontend/src/components/GroupDetails/index.js
@@ -119,12 +119,12 @@ function GroupDetails({ group, user, events, groupId }) {
                     />
                     <NavLink to='/groups' className='displayFlex UblackColor UnoDecoration backButtonTextSize alignCenter'>Back to All Groups</NavLink>
                 </div>
+                
                 <div className="eventGroupComponent">
                     <EventGroupComponent type={type} previewImage={groupPreviewImage} info={info} />
                 </div>
 
-
-                <div key='current' className={`${showCurrentEvents} borderGreen eventListMargin`}>
+                <div key='current' className={`${showCurrentEvents} eventListMargin`}>
                     <EventsDisplayComponent
                         key='current'
                         timeline={'current'} eventsArray={eventsArray[2]}
