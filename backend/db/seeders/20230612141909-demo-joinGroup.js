b1b7006defd67743b4d6e307de64b79da7f0143f
TheSicilian12 <mguidera314@gmail.com>
2023-06-12 10:23:07 -0400

    STOP. Membership table already exists.

---
+++ backend/db/seeders/20230612141909-demo-joinGroup.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+options.tableName = 'JoinGroups';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    options.tableName = "JoinGroups";
+    return queryInterface.bulkInsert(options, [
+      {
+        groupId: 1,
+        userId: 1
+      }
+    ], {})
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
