9cac05d71ba3d52848be0bfb2906014abe393276
TheSicilian12 <mguidera314@gmail.com>
2023-06-23 13:10:45 -0400

    edit event adjusts for past events

--- frontend/src/components/eventGroupComponent/index.js
+++ frontend/src/components/eventGroupComponent/index.js
@@ -286,9 +286,6 @@ export default function EventGroupComponent({ type, previewImage, info }) {
                                     </div>
                                 </div>
                             </div>
-
-
-
                         </div>
                     </div>}
 
